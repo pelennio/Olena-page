a854d98be7e32359700cd307b9c08448d23df0ce
Olena Pohorila <opohorila@atlassian.com>
2019-04-01 13:01:45 -0500

    3 commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,24 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+    if(mySrc === 'images/Olena1.jpg') {
+      myImage.setAttribute ('src','images/Olena2.jpg');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','images/Olena1.jpg');
     }
 }
 
 var header = document.querySelector('h1');
 console.log('verify header');
-
 header.onclick = function() {
     var mySrc1 = header.textContent;
     console.log('set function');
     console.log('mySrc = '+ mySrc1);
-    if(mySrc1 === 'Mozilla is cool') {
+    if(mySrc1 === 'Olena Pohorila') {
       console.log('yes');
       header.textContent = 'Olena!';
     } else {
-      header.textContent='Mozilla is cool';
+      header.textContent='Olena Pohorila';
       console.log('no');
     }
 }
@@ -41,7 +40,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'Welcome to my page, ' + storedName;
 }
 
 myButton.onclick = function() {
